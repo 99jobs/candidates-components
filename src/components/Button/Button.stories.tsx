1e74dcdaba0fe24673cafaa7853d920095a2e739
João <joao@MacBook-Pro.local>
2022-09-28 16:26:47 -0300

    Ajuste no projeto

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -11,7 +11,7 @@ const Template: Story<ButtonProps> = (args) => <Button {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-  label: 'Button Default',
+  label: 'Button',
 }
 
 export const DefaultWithPrefixIcon = Template.bind({})

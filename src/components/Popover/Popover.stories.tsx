84090d8614913bf9e3bb3b9c2e986ce1de770ac5
João <joao@MacBook-Pro.local>
2022-09-28 13:00:28 -0300

    Finalizando teste do componente Popover

--- src/components/Popover/Popover.stories.tsx
+++ src/components/Popover/Popover.stories.tsx
@@ -1,6 +1,8 @@
+import { expect } from '@storybook/jest'
 import { Story } from '@storybook/react'
+import { within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { Button } from '../Button'
-
 import { Popover, PopoverProps } from './index'
 
 export default {
@@ -14,4 +16,24 @@ export const Default = Template.bind({})
 Default.args = {
   triggerNode: <Button label="Open Popup" />,
   contentNode: <p>Hello World!!</p>,
+}
+
+Default.play = async ({ canvasElement }) => {
+  // Cria o canvas
+  const canvas = within(canvasElement)
+
+  // Clica no botão que aciona o popover
+  userEvent.click(canvas.getByRole('button'))
+
+  // Como o popover criado não vam no canvasElement, e sim na body, precisamos "procurar" a body a partir do botão que acionou o popover
+  const body = canvasElement.closest('body')
+  // Cria o canvas da body agora
+  const bodyCanvas = within(body || canvasElement)
+
+  // Procura o popover a partir de uma role
+  const popover = bodyCanvas.getByRole('dialog')
+
+  // Esperamos que o popover tenha sido criado e que ele contenha o elemento passado no parâmetro contentNode
+  expect(popover).toBeInTheDocument()
+  expect(popover.innerHTML).toContain('<p>Hello World!!</p>')
 }

8b19282fe30b869b861f80884daa56ed92616c8a
Camila Arruda <camila.martins@99jobs.com>
2022-10-17 10:59:01 -0300

    Corrigindo os styles

--- src/components/Avatar/Avatar.stories.tsx
+++ src/components/Avatar/Avatar.stories.tsx
@@ -11,4 +11,11 @@ const Template: Story<AvatarOwnProps> = (args) => <Avatar {...args} />
 export const Default = Template.bind({})
 Default.args = {
   fallback: 'CA',
+  // src: 'https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80',
+}
+
+export const Second = Template.bind({})
+Second.args = {
+  src: 'https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80',
+  alt: 'girl',
 }
--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,16 +1,17 @@
 import { ComponentProps } from '@stitches/react'
-import React from 'react'
-import { StyledAvatar, StyledAvatarFallback, StyledAvatarImage } from './style'
+import { AvatarNestedItem, StyledAvatar, StyledAvatarFallback, StyledAvatarImage } from './style'
 
 export interface AvatarOwnProps extends ComponentProps<typeof StyledAvatar> {
   alt?: string
   src?: string
-  fallback?: React.ReactNode
+  fallback?: string
 }
 
-export const Avatar = ({ alt, src, fallback, size, variant, shape, ...props }: AvatarOwnProps) => (
-  <StyledAvatar {...props} size={size} variant={variant} shape={shape}>
-    <StyledAvatarImage alt={alt} src={src} />
-    <StyledAvatarFallback size={size}>{fallback}</StyledAvatarFallback>
-  </StyledAvatar>
+export const Avatar = ({ alt, src, fallback, ...props }: AvatarOwnProps) => (
+  <AvatarNestedItem>
+    <StyledAvatar {...props}>
+      <StyledAvatarImage alt={alt} src={src} />
+      <StyledAvatarFallback>{fallback}</StyledAvatarFallback>
+    </StyledAvatar>
+  </AvatarNestedItem>
 )

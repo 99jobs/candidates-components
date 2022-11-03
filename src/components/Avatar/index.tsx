fbc65cca95312592be5781ff7f9f092c0fd94a6f
Camila Arruda <camila.martins@99jobs.com>
2022-11-03 15:40:01 -0300

    Validação tamanho e formato do file

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable no-console */
 import { ComponentProps } from '@stitches/react'
-import { ReactElement } from 'react'
+import { ReactElement, type ChangeEvent } from 'react'
 import {
   Container,
   StyledAvatar,
@@ -26,18 +27,38 @@ export const Avatar = ({
   sufixIcon,
   label,
   avatar,
+  onChange,
   ...props
-}: AvatarOwnProps) => (
-  <Container>
-    <StyledAvatar {...props}>
-      <StyledAvatarIcon>{avatar}</StyledAvatarIcon>
-      <StyledAvatarImage alt={alt} src={src} />
-      <StyledAvatarFallback>{fallback}</StyledAvatarFallback>
-    </StyledAvatar>
-    <StyledInputLabel>
-      {sufixIcon}
-      <StyledInputField type="file" />
-      {label}
-    </StyledInputLabel>
-  </Container>
-)
+}: AvatarOwnProps) => {
+  // const [userAvatar, setUserAvatar] = useState('')
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { files, value } = e.target
+
+    const allowedFormats = /(.jpg|.jpeg|.png)$/i
+    const maximumSize = 2100000
+
+    if (!allowedFormats.exec(value)) {
+      console.log('Formato incorreto')
+    } else if (files && files[0].size > maximumSize) {
+      console.log('Tamanho incorreto')
+    } else {
+      console.log('OK')
+    }
+  }
+
+  return (
+    <Container>
+      <StyledAvatar {...props}>
+        <StyledAvatarIcon>{avatar}</StyledAvatarIcon>
+        <StyledAvatarImage alt={alt} src={src} />
+        <StyledAvatarFallback>{fallback}</StyledAvatarFallback>
+      </StyledAvatar>
+      <StyledInputLabel>
+        {sufixIcon}
+        <StyledInputField type="file" id="arquivo" onChange={handleChange} />
+        {label}
+      </StyledInputLabel>
+    </Container>
+  )
+}

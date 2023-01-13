742076414af17d1a1f54d1916472b16de7f1b136
João <joao@MacBook-Pro.local>
2023-01-13 11:36:59 -0300

    Corrige componente

--- src/components/OpportunityOpenUntil/index.tsx
+++ src/components/OpportunityOpenUntil/index.tsx
@@ -5,7 +5,5 @@ export interface OpportunityOpenUntilProps
   extends ComponentProps<typeof StyledOpportunityOpenUntil> {}
 
 export const OpportunityOpenUntil = ({ ...props }: OpportunityOpenUntilProps) => (
-  <StyledOpportunityOpenUntil {...props}>
-    Inscrições abertas até <time dateTime="2022-11-28">28/11/2022</time>
-  </StyledOpportunityOpenUntil>
+  <StyledOpportunityOpenUntil {...props} />
 )

37a2f3d947637b35be74c35c4ffabdeea60f714b
João <joao@MacBook-Pro.local>
2023-02-15 15:15:53 -0300

    Ajuste fino header

--- src/components/Header/style.tsx
+++ src/components/Header/style.tsx
@@ -3,7 +3,7 @@ import { styled } from '../../configs/stitches.config'
 export const StyledHeader = styled('header', {
   width: '100%',
   paddingX: 20,
-  paddingY: 28,
+  paddingY: 23,
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-between',

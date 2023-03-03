d0a953dbac49af2bd723d8d85fc4408fc5b3c742
João <joao@MacBook-Pro.local>
2023-03-03 18:47:07 -0300

    Ajuste fino

--- src/components/OpportunityCard/style.tsx
+++ src/components/OpportunityCard/style.tsx
@@ -3,6 +3,7 @@ import { styled } from '../../configs/stitches.config'
 export const StyledOpportunityCard = styled('div', {
   width: '100%',
   maxWidth: 320,
+  flexShrink: 0,
 
   '@md': {
     maxWidth: 560,

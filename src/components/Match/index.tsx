e5068dc072996005e2533c95d62d9c5321cc1461
João <joao@MacBook-Pro.local>
2023-03-22 17:38:50 -0300

    Voltando commit

--- src/components/Match/index.tsx
+++ src/components/Match/index.tsx
@@ -1,6 +1,5 @@
 import { type ComponentProps } from '@stitches/react'
-import Progressbar from 'react-js-progressbar'
-import { colors } from '../../configs/colors'
+// import Progressbar from 'react-js-progressbar'
 import { StyledMatch } from './style'
 
 export interface StyledMatchProps extends ComponentProps<typeof StyledMatch> {
@@ -12,14 +11,14 @@ export interface StyledMatchProps extends ComponentProps<typeof StyledMatch> {
 
 export const Match = ({ match, ...props }: StyledMatchProps) => (
   <StyledMatch {...props}>
-    <Progressbar
+    {/* <Progressbar
       input={match}
       trailColor="none"
       trailWidth={12}
       pathWidth={12}
       pathColor={colors.greenSystem}
       textStyle={{ fill: '#fff', fontWeight: 700, fontSize: 58 }}
-    />
+    /> */}
     match
   </StyledMatch>
 )
